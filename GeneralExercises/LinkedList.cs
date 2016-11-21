8bd7f39d8a5fc8067bffde3e64546c8e7e7263fb
ClausJohansen <clausfjohansen@gmail.com>
2016-11-21 14:38:39 +0100

    Linked list exercise - in progress

---
+++ GeneralExercises/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralExercises
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private Node firstElement;
+
+        public void Add<T>(T element)
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Node
+        {
+            protected T Value { get; set; }
+            protected Node Next { get; set; }
+
+            protected Node(T value)
+            {
+                this.Value = value;
+            }
+        }
+    }
+}
--- GeneralExercises/Program.cs
+++ GeneralExercises/Program.cs
@@ -10,67 +10,81 @@ namespace GeneralExercises
     {
         static void Main(string[] args)
         {
-            // Generics opgave.
-
-            List<int> listOfNumbers = new List<int> { 5, 4, 3, 2, 1 };
-            List<int> listOfnumbersReversed = Generics.reverseList(listOfNumbers);
-
-            foreach (int number in listOfNumbers)
-            {
-                Console.Write(number + " ");
-            }
-
-            Console.WriteLine();
-
-            foreach (int number in listOfnumbersReversed)
-            {
-                Console.Write(number + " ");
-            }
-
-            Console.WriteLine();
-
-            List<string> listOfStrings = new List<string> { "Fem", "Fire", "Tre", "To", "En" };
-            List<string> listOfReversedStrings = Generics.reverseList(listOfStrings);
-
-            foreach (string word in listOfStrings)
-            {
-                Console.Write(word + " ");
-            }
-
-            Console.WriteLine();
-
-            foreach (string word in listOfReversedStrings)
-            {
-                Console.Write(word + " ");
-            }
-
-            Console.WriteLine();
-
-            // Lav en generic liste af strings. Udskriv alle elementer.
-
-            List<int> intList = new List<int> { 23, 2, 87, 9000 };
-
-            foreach(int number in intList)
-            {
-                Console.Write(number + " ");
-            }
-
-            Console.WriteLine();
-
-
-            // Lav en generic liste af ints. Udskriv alle elementer.
-
-            List<string> stringList = new List<string> { "Ged", "Dobbeltdækker", "Syngende", "Bly" };
-
-            foreach (string word in stringList)
-            {
-                Console.Write(word + " ");
-            }
-
-            Console.WriteLine();
-
-
+            // GenericsExercise();
+            LinkedListExercise();
             Console.ReadKey();
+        }
+
+        private static void GenericsExercise()
+        {
+             // Generics opgave.
+
+             List<int> listOfNumbers = new List<int> { 5, 4, 3, 2, 1 };
+             List<int> listOfnumbersReversed = Generics.reverseList(listOfNumbers);
+
+             foreach (int number in listOfNumbers)
+             {
+                 Console.Write(number + " ");
+             }
+
+             Console.WriteLine();
+
+             foreach (int number in listOfnumbersReversed)
+             {
+                 Console.Write(number + " ");
+             }
+
+             Console.WriteLine();
+
+             List<string> listOfStrings = new List<string> { "Fem", "Fire", "Tre", "To", "En" };
+             List<string> listOfReversedStrings = Generics.reverseList(listOfStrings);
+
+             foreach (string word in listOfStrings)
+             {
+                 Console.Write(word + " ");
+             }
+
+             Console.WriteLine();
+
+             foreach (string word in listOfReversedStrings)
+             {
+                 Console.Write(word + " ");
+             }
+
+             Console.WriteLine();
+
+             // Lav en generic liste af strings. Udskriv alle elementer.
+
+             List<int> intList = new List<int> { 23, 2, 87, 9000 };
+
+             foreach(int number in intList)
+             {
+                 Console.Write(number + " ");
+             }
+
+             Console.WriteLine();
+
+
+             // Lav en generic liste af ints. Udskriv alle elementer.
+
+             List<string> stringList = new List<string> { "Ged", "Dobbeltdækker", "Syngende", "Bly" };
+
+             foreach (string word in stringList)
+             {
+                 Console.Write(word + " ");
+             }
+
+             Console.WriteLine();
+        }
+
+        private static void LinkedListExercise()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            // list.FirstElement = new Node(1);
+
+
+
         }
     }
 }
