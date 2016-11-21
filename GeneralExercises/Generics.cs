3cb110efb045e6994389ef027d775a6f39f7dd9b
ClausJohansen <clausfjohansen@gmail.com>
2016-11-21 11:34:41 +0100

    Generics exercise

---
+++ GeneralExercises/Generics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralExercises
+{
+    class Generics
+    {
+        public static List<T> reverseList<T> (List<T> input)
+        {
+            List<T> result = new List<T>(input);
+            result.Reverse();
+
+            return result;
+        }
+    }
+}
--- GeneralExercises/Program.cs
+++ GeneralExercises/Program.cs
@@ -10,6 +10,42 @@ namespace GeneralExercises
     {
         static void Main(string[] args)
         {
+            // Generics opgave.
+
+            List<int> listOfNumbers = new List<int> { 5, 4, 3, 2, 1 };
+            List<int> listOfnumbersReversed = Generics.reverseList(listOfNumbers);
+
+            foreach (int number in listOfNumbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int number in listOfnumbersReversed)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+
+            List<string> listOfStrings = new List<string> { "Fem", "Fire", "Tre", "To", "En" };
+            List<string> listOfReversedStrings = Generics.reverseList(listOfStrings);
+
+            foreach (string word in listOfStrings)
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (string word in listOfReversedStrings)
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
